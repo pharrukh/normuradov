6aace28e6f6c9cc0fd7822a2bdbb7d78947bd5bc
Eric Rovell <ericrovell@gmail.com>
2019-08-23 00:11:26 +0300

    Socials Component, wip, links object-file created to parse information

---
+++ src/components/socials/links.js
@@ -0,0 +1,33 @@
+export const links = [
+
+  {
+    name: "mail",
+    link: "mailto:farrukh.normuradov@gmail.com",
+    icon: "",
+  },
+
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/farrukh-normuradov/",
+    icon: "",
+  },
+
+  {
+    name: "StackOverflow",
+    link: "https://stackoverflow.com/users/3407539/farrukh-normuradov",
+    icon: "",
+  },
+
+  {
+    name: "Telegram",
+    link: "https://t.me/UzFeedbackBot",
+    icon: "",
+  },
+
+  {
+    name: "GitHub",
+    link: "https://github.com/pharrukh",
+    icon: "",
+  },
+
+];
