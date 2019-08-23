cb2a849f12c0f46638bba870b7e867c4dcdec65e
Eric Rovell <ericrovell@gmail.com>
2019-08-23 11:18:32 +0300

    added key for react to Social Component

--- src/components/socials/Socials.jsx
+++ src/components/socials/Socials.jsx
@@ -8,7 +8,7 @@ import { links } from "./links";
 const Socials = () => (
   <div className="socials">
     {links.map( (link, i) => (
-      <a href={links.link}>
+      <a href={links.link} key={i}>
         <svg className="icon">
           <use href={link.icon} />
         </svg>
