2f83791e9d04912cec07aecd17e9d6f2a58e4f47
Eric Rovell <ericrovell@gmail.com>
2019-08-24 09:59:20 +0300

    Layout Component with IconSystem, Header and Footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import IconSystem from "components/IconSystem/IconSystem";
+import Header from "components/Layout/header/Header";
+import Footer from "components/Layout/footer/Footer";
+
+import "styles/pages/index.scss";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,24 +27,12 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <div id="app">
+      <IconSystem />
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+      <main>{children}</main>
+      <Footer />
+    </div>
   )
 }
 
