57cb3a06a9bf8678ab864c8d4129ccaf0c3e709e
Eric Rovell <ericrovell@gmail.com>
2019-08-24 10:01:49 +0300

    making absolute imports possible, creating pages for md files

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,55 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require('path');
+
+// absolute modules exports
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
+  })
+}
+
+// creting pages from .md files
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions;
+  const postTemplate = path.resolve("src/templates/BlogPost/BlogPost.jsx");
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+        edges {
+          node {
+            id
+            html
+            frontmatter {
+              path
+              date
+              title
+              keywords
+              description
+              author
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: postTemplate,
+      context: {},
+    })
+  })    
+  
+}
