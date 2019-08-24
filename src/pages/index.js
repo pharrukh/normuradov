f459b4172c51466ee0fe6ebbf98c50bbae1dbcb4
Eric Rovell <ericrovell@gmail.com>
2019-08-24 10:00:15 +0300

    getting list of posts on the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,56 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useEffect } from "react";
+import { Link, graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
 
-export default IndexPage
+  const setTheme = () => {
+    const theme = localStorage.getItem("lastChosenTheme");
+    (theme)
+      ? document.documentElement.setAttribute("data-theme", theme)
+      : document.documentElement.setAttribute("data-theme", "light")
+  }
+
+  useEffect(setTheme);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {data.allMarkdownRemark.edges.map(post => (
+        <div className="post" key={post.node.id}>
+          <time dateTime={post.node.frontmatter.date}>
+            {post.node.frontmatter.date}
+          </time>
+          <Link to={post.node.frontmatter.path}>
+            {post.node.frontmatter.title}
+          </Link>
+        </div>
+      ))}
+    </Layout>
+  );
+  
+}
+
+export const pageQuery = graphql`
+  query BlogIndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            path
+            date(formatString: "YYYY-MM-DD")
+            title
+            keywords
+            description
+            author
+          }
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage;
