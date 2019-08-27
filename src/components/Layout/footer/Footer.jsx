a5133a8a15cf15c2ad2950724f63cf6246c1b831
Eric Rovell <ericrovell@gmail.com>
2019-08-27 16:13:56 +0300

    Socials now moved into Footer

--- src/components/Layout/footer/Footer.jsx
+++ src/components/Layout/footer/Footer.jsx
@@ -3,14 +3,16 @@ import { Link } from "gatsby";
 
 import "./footer.scss";
 
+import Socials from "components/socials/Socials";
 import LangSwitcher from "components/langSwitcher/LangSwitcher";
 
 const Footer = (props) => (
   <footer>
+    <Socials />
     <div className="copyright">
       Copyright © {new Date().getFullYear()}, Farrukh Normuradov
     </div>
-    <div className="links">
+    <div className="language">
       <LangSwitcher switchLang={props.switchLang} />
     </div>
   </footer>
