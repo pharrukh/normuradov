659aa1c1a3c0beb546c0dc4f0f2380ce0ad3c64e
pharrukh <farrukh.normuradov@gmail.com>
2021-01-30 17:17:15 +0100

    feat: exclude url from routing for react project

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,12 +121,11 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+
+    {
+      resolve: 'gatsby-plugin-exclude',
+      options: { paths: ['/product/book-shelves/**'] },
     }
-
-
-    
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
