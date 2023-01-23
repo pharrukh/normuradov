82c38127aacc55aa467ee5bebab2c623b7b01336
Farrukh Normuradov <farrukh.normuradov@gmail.com>
2023-01-23 21:32:50 +0100

    fix: udpate url of absolute links

--- _data/site.js
+++ _data/site.js
@@ -1,6 +1,6 @@
 const isDev = process.env.ELEVENTY_ENV === 'development';
 
-const baseUrl = isDev ? `localhost:8080` : `https://normuradov.com/`;
+const baseUrl = isDev ? `localhost:8080` : `https://www.normuradov.com/`;
 
 const site = {
   title: "normuradov farrukh's personal blog",
